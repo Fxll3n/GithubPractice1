a0fbd728cc99de49c50d430aee3acf250280894b
Santo <smathew7247@stu.d214.org>
2024-04-10 14:47:48 -0500

    Hellooooo

--- ContentView.swift
+++ ContentView.swift
@@ -3,7 +3,8 @@ import SwiftUI
 struct ContentView: View {
     
     @State var number1: Int?
-    
+    @State var number2: Int?
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
